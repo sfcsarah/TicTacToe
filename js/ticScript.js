5aee9920ab62b3c067ae271e4d88ace739052aff
sfcsarah <sfc.sarah@gmail.com>
2016-01-25 09:43:04 -0800

    first commit, partial code

---
+++ js/ticScript.js
@@ -0,0 +1,5 @@
+window.alert("JS scripts loading!");
+console.log("JS console works!");
+
+// document.querySelectorAll('div.square')[0].innerText;
+//above lets you get the content of the div. it's "" for an empty box
